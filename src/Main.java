6abef013e726c5ad7b28ccde6259fa22706133db
JohnYanni12 <mariamjna1@gmail.com>
2022-12-20 18:32:57 -0600

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem ss = new SolarSystem();
+        ss.sun=12;
+        ss.moon = 10;
+        ss.planet = 90;
+
+
+        FeatureSun Fxron=new FeatureSun();
+        Fxron.color="green";
+        Fxron.size="4382742km";
+        Fxron.temp="-23478274372k";
+    }
+}
